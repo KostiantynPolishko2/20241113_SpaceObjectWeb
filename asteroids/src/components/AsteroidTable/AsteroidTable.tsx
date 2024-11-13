73d32d1b29628fff585ef5e88e8314552b3bbbc4
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-11-13 18:11:30 +0200

    4 added tested FC AsteroidTable

---
+++ asteroids/src/components/AsteroidTable/AsteroidTable.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { AsteroidTableWrapper } from './AsteroidTable.styled';
+
+interface AsteroidTableProps {}
+
+const AsteroidTable: FC<AsteroidTableProps> = () => (
+ <AsteroidTableWrapper>
+    AsteroidTable Component
+ </AsteroidTableWrapper>
+);
+
+export default AsteroidTable;
--- asteroids/src/index.tsx
+++ asteroids/src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import AsteroidTable from './components/AsteroidTable/AsteroidTable';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,10 +11,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App text='hello world!'/>
+    <AsteroidTable/>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
