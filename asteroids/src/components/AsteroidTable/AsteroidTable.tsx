4dcd00595343189f8b37f86d090653d71db3f8b3
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-11-13 18:20:56 +0200

    5 added image path to Azure Storage

--- asteroids/src/components/AsteroidTable/AsteroidTable.tsx
+++ asteroids/src/components/AsteroidTable/AsteroidTable.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react';
 import { AsteroidTableWrapper } from './AsteroidTable.styled';
+import '../../App.css'
 
-interface AsteroidTableProps {}
+interface AsteroidTableProps {
+   img_path: string
+}
 
-const AsteroidTable: FC<AsteroidTableProps> = () => (
+const AsteroidTable: FC<AsteroidTableProps> = (props) => (
  <AsteroidTableWrapper>
     AsteroidTable Component
+    <img src={props.img_path} className='App-logo' alt='logo'/>
  </AsteroidTableWrapper>
 );
 
--- asteroids/src/index.tsx
+++ asteroids/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 import AsteroidTable from './components/AsteroidTable/AsteroidTable';
 
 const root = ReactDOM.createRoot(
@@ -11,11 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App text='hello world!'/>
-    <AsteroidTable/>
+    <AsteroidTable img_path='https://docfiles.blob.core.windows.net/files/images/TQ1_AzureSQLDatabaseOfflineMigration_00.png'/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
