ff202f53874254fd0b20ce61805830b2d4664374
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-11-17 14:13:36 +0200

    21 reconnect to web api spaceobjectai2

--- asteroids/src/components/AsteroidCard/api.ts
+++ asteroids/src/components/AsteroidCard/api.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 export const fetchImagePath = async (asteroidName: string): Promise<string> => {
 
     const asteroidImage = axios.create({
-        baseURL: 'https://spaceobjectaiserver.azurewebsites.net/api/AsteroidImage/asteroid-image',
+        baseURL: 'https://spaceobjectai2.azurewebsites.net/api/AsteroidImage/asteroid-image',
+        //baseURL: 'https://spaceobjectaiserver.azurewebsites.net/api/AsteroidImage/asteroid-image',
         //baseURL: 'https://localhost:7155/api/AsteroidImage/asteroid-image',
         method: 'get',
         responseType: 'json',
